3b53da34c43e91db21bf7b47198d2929c7e293a1
Cauê Alves <ceasbz2@gmail.com>
2017-08-24 19:48:56 -0300

    (refactor): extracted wrapper links to function

--- tests/blogium.js
+++ tests/blogium.js
@@ -4,6 +4,16 @@ import fetchMock from 'fetch-mock';
 describe('Blogium', () => {
   let blog;
 
+  const wrapperLinks = (href, target) => {
+    for(let i = 0; i < 10; i++) {
+      let element = document.createElement('a');
+      element.href = href;
+      element.setAttribute('target', target);
+      global.wrapper.appendChild(element);
+    }
+  }
+
+
   beforeEach(() => {
     fetchMock.get('*', { "hello": "world"});
 
@@ -21,9 +31,7 @@ describe('Blogium', () => {
     });
   });
 
-  afterEach(() => {
-      fetchMock.restore();
-  });
+  afterEach(() => fetchMock.restore());
 
   describe('#constructor', () => {
     it('check if Blog is an instance of Blogium', () => {
@@ -44,37 +52,23 @@ describe('Blogium', () => {
         content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto saepe temporibus iure odio eaque dolor."
       };
 
-      let post = blog.blogPostTemplate(mockPost);
+      const post = blog.blogPostTemplate(mockPost);
       assert.equal(typeof post, 'string', 'post must be a string');
     });
   });
 
   describe('#setLinkTarget', () => {
+    afterEach(() => global.wrapper.innerHTML = '');
+
     it('should change target of outbound links to _blank', () => {
-      for(let i=0; i < 10; i++) {
-        let newLink = document.createElement('a');
-        newLink.href = 'github.com';
-        newLink.setAttribute('target', '_self');
-        global.wrapper.appendChild(newLink);
-      }
-
+      wrapperLinks('github.com', '_blank');
       blog.setLinkTarget(global.wrapper);
-
       assert.equal(global.wrapper.querySelector('a').getAttribute('target'), '_blank', 'target is blank');
-
-      global.wrapper.innerHTML='';
     });
 
     it('should keep target of inbound links as _self', () => {
-      for(let i=0; i < 10; i++) {
-        let otherLink = document.createElement('a');
-        otherLink.href = 'https://atilafassina.com';
-        otherLink.setAttribute('target', '_self');
-        global.wrapper.appendChild(otherLink);
-      }
-
+      wrapperLinks('https://atilafassina.com', '_self');
       blog.setLinkTarget(global.wrapper);
-
       assert.equal(global.wrapper.querySelector('a').getAttribute('target'), '_self', 'target is _self');
     });
   });
